f74ba4e9e8dbe4079b036a38bfbfca0c18166d3a
mostafijur rahman <mostafijur.rahman@v2.ltd>
2022-12-21 10:04:44 +0600

    fix: realtime result reorder

--- src/services/Socket.ts
+++ src/services/Socket.ts
@@ -1,4 +1,7 @@
+import { Calculation } from "./../models/Calculation";
 import { Server } from "socket.io";
+import { CalculationModel } from "../models/Calculation";
+import { ObjectId } from "mongoose";
 
 export class SocketServer {
   io: Server;
@@ -7,7 +10,7 @@ export class SocketServer {
   constructor(server: any) {
     this.io = new Server(server, {
       cors: {
-        origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
+        origin: ["http://127.0.0.1:3001", "http://localhost:3001"],
         methods: ["GET", "POST", "DELETE", "OPTION", "PATCH"],
       },
     });
@@ -28,6 +31,26 @@ export class SocketServer {
 
       socket.on("disconnect", () => {
         console.log("disconneced: ", socket.id);
+      });
+
+      socket.on("reorder", async (data: { id: ObjectId; index: number }[]) => {
+        try {
+          await CalculationModel.updateOne(
+            { _id: data[0].id },
+            { $set: { index: data[0].index } }
+          );
+          await CalculationModel.updateOne(
+            { _id: data[1].id },
+            { $set: { index: data[1].index } }
+          );
+
+          this.io.emit("reordered", {
+            [data[0].id.toString()]: data[0].index,
+            [data[1].id.toString()]: data[1].index,
+          });
+        } catch (error) {
+          throw error;
+        }
       });
     });
   }
